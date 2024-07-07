882de6f2b09bc73da0e5991076657ea6ee348526
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-07-07 15:43:30 -0300

    Update <RA>-A7-Q2.sql

--- Trabalho Atividade 7/<RA>-A7-Q2.sql
+++ Trabalho Atividade 7/<RA>-A7-Q2.sql
@@ -11,3 +11,31 @@ dlocal varchar(400) not null,
 primary key (dnumero,dlocal),
 foreign key (dnumero) references departamento(dnumero)
 );
+
+create table projeto(
+projnumero int4 not null,
+projnome varchar(300),
+projlocal varchar(300),
+dnumero int4,
+primary key (projnumero),
+foreign key (dnumero) references departamento(dnumero)
+);
+
+create table funcionario(
+cpf varchar(20)not null,
+fnome varchar(300),
+datanasc date,
+endereco varchar(600),
+dnumero int4,
+primary key(cpf),
+foreign key (dnumero) references departamento(dnumero)
+);
+
+create table projeto_alocacao(
+cpf varchar(20) not null,
+projnumero int4 null,
+horas float8,
+primary key(cpf,projnumero),
+foreign key (cpf) references funcionario(cpf),
+foreign key (projnumero) references projeto(projnumero)
+);
